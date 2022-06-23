2d49d21d35faaf651fbe999d4aa607c487271e85
Jakman2371 <99205529+Jakman2371@users.noreply.github.com>
2022-06-23 10:33:57 -0700

    ye

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
 let config = {
     type: Phaser.CANVAS,
     width: 640,
-    height: 480,
-    scene: [ Menu, Play ]
+    height:480,
+    scale: {autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [Menu, Play]
+  
 }
-
-let game = new Phaser.Game(config);
-
-// set UI sizes
-let borderUISize = game.config.height / 15;
-let borderPadding = borderUISize / 3;
-
-// reserve keyboard variables
+let game = new Phaser.Game(config); 
 let keyF, keyR, keyLEFT, keyRIGHT;
+
+let borderUISize = game.config.height / 15; 
+let borderPadding = borderUISize / 3; 
+
+/*
+Points:
+- Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+- Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
+- Display the time remaining (in seconds) on the screen (10)
+- Create a new animated sprite for the Spaceship enemies (10)
+With Help From:
+- Joann Wong
+- CMPM 120 Discord
+*/
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,60 +1,72 @@
 class Menu extends Phaser.Scene {
-    constructor() {
-        super("menuScene");
-    }
+  constructor() {
+      super("menuScene");
+  }
 
-    preload() {
-        // load audio
-        this.load.audio('sfx_select', './assets/blip_select12.wav');
-        this.load.audio('sfx_explosion', './assets/explosion38.wav');
-        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
-    }
+  preload() {
+      this.load.audio('sfx_select', './assets/Select.wav');
+      this.load.audio('sfx_explosion', './assets/ringring.mp3');
+      this.load.audio('sfx_rocket','./assets/heart.wav'); 
+      this.load.audio('bgm','./assets/bgm.mp3');
+      this.load.image('titlescreen', './assets/titlescreen.png');
 
-    create() {
-        // menu text configuration
-        let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-            fixedWidth: 0
-        }
-        
-        // show menu text
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+      
+  }
 
-        // define keys
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-    }
-
-    update() {
-        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
-          // Novice mode
-          game.settings = {
-            spaceshipSpeed: 3,
-            gameTimer: 60000    
-          }
-          this.sound.play('sfx_select');
-          this.scene.start("playScene");    
-        }
-        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
-          // Expert mode
-          game.settings = {
-            spaceshipSpeed: 4,
-            gameTimer: 45000    
-          }
-          this.sound.play('sfx_select');
-          this.scene.start("playScene");    
-        }
+  create() {
+      
+    let menuConfig = {
+          fontFamily: 'Garamond',
+          fontSize: '28px',
+          backgroundColor: '#F3B141',
+          color: '#843605',
+          align: 'right',
+          padding: {
+              top: 5,
+              bottom: 5, 
+          },
+          fixedWidth:0 
       }
+      
+      this.bgm = this.sound.add("bgm");
+
+      var bgmConfig = {
+        mute: false,
+        volume: 1,
+        rate: 1,
+        detune: 0,
+        seek: 0,
+        loop: true,
+        delay: 0
+      }
+      
+      this.bgm.play(bgmConfig);
+
+      this.mainMenu = this.add.tileSprite(0, 0, 640, 480, 'titlescreen').setOrigin(0, 0);
+
+      keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+      keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+  }
+  update() {
+      if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+        // easy mode
+        game.settings = {
+          spaceshipSpeed: 3,
+          gameTimer: 60000    
+        }
+        this.sound.play('sfx_select');
+        this.scene.start('playScene');    
+      }
+      if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+        // hard mode
+        game.settings = {
+          spaceshipSpeed: 8,
+          gameTimer: 45000    
+        }
+        this.sound.play('sfx_select');
+        this.scene.start('playScene');    
+      }
+    }
+    
+    
 }
